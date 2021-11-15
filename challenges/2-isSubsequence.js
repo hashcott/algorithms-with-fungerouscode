4b26c59df7721f4fd85cf9d4a12487c35c25fc6a
insecures <duchanhstyle@gmail.com>
2021-11-15 14:55:09 +0700

    fix name

---
+++ challenges/2-isSubsequence.js
@@ -0,0 +1,16 @@
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world"));
+
+console.log(isSubsequence("abc", "adsdcsbc"));
+console.log(isSubsequence("abc", "adsdcscb"));
